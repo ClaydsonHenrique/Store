d7a7f3b11d82f083e50b93f0aba3ca9201903fa0
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-07-15 15:05:10 -0300

    ajustando erro Orders Controllers

--- app/backend/src/controllers/Orders.controller.ts
+++ app/backend/src/controllers/Orders.controller.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 import getAllOrdersService from '../services/Orders.services';
 
-const getAllOrders = async (res:Response, req:Request) => {
+const getAllOrders = async (req: Request, res: Response) => {
   const authorizationHeader = req.get("Authorization");
-  if(!authorizationHeader){
-    return res.status(401).json({message: "Token not found"});
+  if (!authorizationHeader) {
+    return res.status(401).json({ message: "Token not found" });
   }
   const token = authorizationHeader.split(" ")[1];
   const resultOrders = await getAllOrdersService(token);
-  return res.status(resultOrders.status).json(resultOrders.data);
-}
+  const { status, data } = resultOrders;
+  return res.status(status).json(data);
+};
 
 export default getAllOrders

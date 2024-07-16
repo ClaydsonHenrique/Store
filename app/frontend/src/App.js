559f55cb01f512acd18bbb7e4864ff8143f45a2e
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-07-16 16:07:03 -0300

    refazendo front-end do projeto

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx'
-import ProductDetalhe from "./pages/ProductDetalhes.jsx";
-import Filteres from "./Components/Filteres.jsx";
+
 
 
 function App() {
 
   return (
     <div className="App">
-      <Filteres></Filteres>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detalhes/:id" element={<ProductDetalhe />} />
-      </Routes>
+      <h1> App</h1>
     </div>
   );
 }

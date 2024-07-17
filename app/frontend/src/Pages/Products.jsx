b8958cb5519042dd8ce1642da6928bee2c2b3c66
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-07-17 18:14:14 -0300

    ajustando pra que seja renderizado 20 produtos

--- app/frontend/src/Pages/Products.jsx
+++ app/frontend/src/Pages/Products.jsx
@@ -18,13 +18,13 @@ function Products() {
 
   const renderProduct = () => {
     if (products) {
-      return products.map((product) => {
+      return products.slice(0, 20).map((product) => {
         return (
           <div className="card" key={product.id}>
             <img src={product.image} alt={product.name} />
             <div className="card-body">
               <h5 className="card-title">{product.name}</h5>
-              <img src={product.images[0]} />
+              <img src={product.images[0]} alt={product} />
               <p className="card-text">{product.description}</p>
               <Link to={`/products/${product.id}`} className="btn btn-primary">Detalhes</Link>
             </div>
@@ -40,6 +40,7 @@ function Products() {
           <nav>
             <ul>
               <li><Link to='/login'>login</Link></li>
+              <li><Link to='/singIn'>SingIN</Link></li>
             </ul>
           </nav>
           <div>Escolha seus Produtos </div>

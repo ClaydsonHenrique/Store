11c6df686b4505015e5ba9ec7f174cbd1077cac1
Claydson Henrique <claydsonhenriquebs@gmail.com>
2025-03-20 17:48:30 -0300

    removendo todos arquivos

--- app/frontend/src/Components/ButtonCart.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ButtonCart() {
-  return (
-    <button className='min-w-32 w-full rounded-md bg-blue-600 text-white py-2 px-4 text-sm transition-all shadow-md hover:bg-blue-700 border border-transparent text-center text-sm text-slate-600 transition-all shadow-md hover:shadow-lg focus:bg-white/90 focus:shadow-none active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'>Add cart</button>
-  )
-}
